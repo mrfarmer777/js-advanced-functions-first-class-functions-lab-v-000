8e602439f74e20f631ec4e3fb17ac39de6693e8f
Matt Farmer <mrfarmer777@gmail.com>
2018-07-27 04:00:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 // Code your solution in this file!
 const returnFirstTwoDrivers=function(drvs){
   return drvs.slice(0,2);
-}
+};
+
+const returnLastTwoDrivers=function(drvs){
+  return drvs.slice(-2,drvs.length);
+};
