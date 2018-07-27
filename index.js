05702fcc6a90222260ef71ad82a8c866c7e0eb94
Matt Farmer <mrfarmer777@gmail.com>
2018-07-27 04:06:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 // Code your solution in this file!
+
+//A variable that stores a function expression.
 const returnFirstTwoDrivers=function(drvs){
   return drvs.slice(0,2);
 };
 
+//Another variable that stores a function expression.
 const returnLastTwoDrivers=function(drvs){
   return drvs.slice(-2);
 };
 
+//An array of function objects
 const selectingDrivers=[returnFirstTwoDrivers,returnLastTwoDrivers];
 
+
+//A named function statement (described, but not referenced or invoked)
 function createFareMultiplier(mult){
   return function(fare){
     return fare*mult;
@@ -18,6 +24,8 @@ function createFareMultiplier(mult){
 const fareDoubler=createFareMultiplier(2);
 const fareTripler=createFareMultiplier(3);
 
+
+//Another function statement
 function selectDifferentDrivers(drvs,selectFunc){
   return selectFunc(drvs);
 }
